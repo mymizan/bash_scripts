fb7ac0de5decb8b30801d08d51701d842e175afa
M Yakub Mizan <ctgcoder@gmail.com>
2016-10-19 19:37:39 +0600

    updated script, added readme

---
+++ README.md
@@ -0,0 +1,17 @@
+====
+BASH Backup Script
+====
+<p> with daily, weeky and monthly backup retention policy. Offers incremental backups and auto purging of older backup to free up disk space
+<br /> Author: M Yakub Mizan
+<br /> URL: http://mymizan.rocks/
+</p>
+
+===
+Installation
+===
+1. Clone the git repository on your server
+2. Adjust the confiuration variables in backup.sh
+3. Make it executable with chmod a+x backup.sh
+4. Add it to your crontab to run automatically 
+
+
--- backup.sh
+++ backup.sh
@@ -1,23 +1,65 @@
 #!/usr/bin/env bash
 #
-# BASH Backup Script
+# BASH Backup Script with daily, weeky and monthly backup retention policy.
+# Offers incremental backups and auto purging of older backup to free up
+# disk space
+# @author M Yakub Mizan
+# @url http://mymizan.rocks/
+# @email ctgcoder@gmail.com
+#
 #
 
-# directory to backup
-SOURCE_DIR="/Users/myakubmizan/CODE/www_root/"
+################################################################################
+######      CHANGE SOURCE_DIR AND BACKUP_DIR TO YOUR DESIRED ONES      #########
+################################################################################
 
-# directory to store backups
-BACKUP_DIR="/Users/myakubmizan/CODE/test/"
+SOURCE_DIR="/Users/myakubmizan/CODE/www_root/" # directory to backup
+BACKUP_DIR="/Users/myakubmizan/CODE/test/"     # backup destination
+NUMBER_OF_BACKUPS=7 # Total number of daily backups to keep
+DO_INCREMENTAL_BACKUPS=true # set to false to take full daily backups
+                            # otherwise leave true to take daily incremental backup
+TAKE_WEEKLY_FULL_BACKUP=true # Take a full backup every week
+SILENT_TAR=1 # hide tar output from terminal. If set to 0 tar runs in verbose mode
 
-# Get date string
-# will be appended after the file name
+
+
+################################################################################
+###### DO NOT CHANGE ANYTHIG BELOW UNLESS YOU KNOW WHAT YOU ARE DOING! #########
+################################################################################
+
+# Check if source directory exists
+if [ ! -d $SOURCE_DIR ]; then
+  echo "Source directory doesn't exist! Refusing to proceed."
+  exit
+fi
+
+if [ $SILENT_TAR -eq 1 ]; then
+  SILENT_TAR=' '
+else
+  SILENT_TAR='v'
+fi
+
+# Get date string to append after the file name
 function getDate(){
   date +"%d-%m-%Y_%H-%M-%S"
 }
 
+#Remove daily backups greater than the number set in NUMBER_OF_BACKUPS.
+#Mothly backups and weekly backups are calculated seperately. Weekly and
+#monthly backups are never deleted if DO_INCREMENTAL_BACKUPS is set to true
 function deleteOlderBackups(){
-  # clean backups older than 7 days
-  echo ""
+  COUNT=1
+  for FILENAME in $(ls -1 $BACKUP_DIR | sort -r); do
+    if [ $NUMBER_OF_BACKUPS -gt $COUNT ]; then
+      rm "$BACKUP_DIR/$FILENAME"
+      echo "Removed Backup: $FILENAME"
+    fi
+    COUNT=$COUNT+1
+  done
+}
+
+function purgeIncompleteBackups(){
+  echo "Purging incomplete backups...."
 }
 
 echo "Backup Started...."
@@ -39,7 +81,7 @@ fi
 
 #start backup
 BACKUP_LOCATION="$BACKUP_DIR""$BACKUP_NAME"
-tar cvfz $BACKUP_LOCATION $SOURCE_DIR
+tar cfz$SILENT_TAR $BACKUP_LOCATION $SOURCE_DIR
 cat <<- __EOF__
 
 
